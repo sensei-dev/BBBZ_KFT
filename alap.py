3965cf1484f3142c4659d2f48cde5e33c6e7987b
z0l1 <116083393+IamDaddyZ0l1@users.noreply.github.com>
2022-10-25 13:55:34 +0200

    Create alap.py

---
+++ alap.py
@@ -0,0 +1,16 @@
+#Melyik szám a nagyobb?
+
+
+szam1 = input("Kerem a szamot: ")
+szam2 = input("Kerem a szamot: ")
+
+
+
+if szam1 > szam2:
+  print("Az elso szam a nagyobb!")
+
+if szam2 > szam1:
+  print("A masodik szam a nagyobb!")
+
+if szam1 == szam2:
+  print("A ket szam pontosan ugyanaz!")
